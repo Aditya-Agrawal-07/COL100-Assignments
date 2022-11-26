0d765c4414dd4a33f9b79e38fc16d62150cb23fa
Aditya Agrawal <106912018+Aditya-Agrawal-07@users.noreply.github.com>
2022-11-26 21:33:48 +0545

    Update 2021AM10198-q.py

--- 2021AM10198_ASSIGNMENT8/2021AM10198-q.py
+++ 2021AM10198_ASSIGNMENT8/2021AM10198-q.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 start_time = datetime.now()
 from typing import Tuple, List
-# No other imports allowed
-
-# PART OF THE DRIVER CODE
 
 def input_sudoku() -> List[List[int]]:
 	"""Function to take input a sudoku from stdin and return
@@ -24,8 +21,6 @@ def print_sudoku(sudoku:List[List[int]]) -> None:
 		for j in range(9):
 			print(sudoku[i][j], end = " ")
 		print()
-
-# You have to implement the functions below
 
 def get_block_num(sudoku:List[List[int]], pos:Tuple[int, int]) -> int:
 	"""This function takes a parameter position and returns
@@ -83,29 +78,15 @@ def get_block(sudoku:List[List[int]], x: int) -> List[int]:
 	return l
 	
 def get_row(sudoku:List[List[int]], i: int)-> List[int]:
-	"""This function takes an integer argument i and then returns
-	the ith row. Row indexing have been shown above.
-	"""
-	# your code goes here
 	return sudoku[i-1]
 
 def get_column(sudoku:List[List[int]], x: int)-> List[int]:
-	"""This function takes an integer argument i and then
-	returns the ith column. Column indexing have been shown above.
-	"""
-	# your code goes here
 	l=[]
 	for i in sudoku:
 		l.append(i[x-1])
 	return l
 
 def find_first_unassigned_position(sudoku : List[List[int]]) -> Tuple[int, int]:
-	"""This function returns the first empty position in the Sudoku. 
-	If there are more than 1 position which is empty then position with lesser
-	row number should be returned. If two empty positions have same row number then the position
-	with less column number is to be returned. If the sudoku is completely filled then return `(-1, -1)`.
-	"""
-	# your code goes here
 	c=d=e=0
 	for i in range(9):
 		for j in range(9):
@@ -120,11 +101,6 @@ def find_first_unassigned_position(sudoku : List[List[int]]) -> Tuple[int, int]:
 		return (c,d)
 
 def valid_list(lst: List[int])-> bool:
-	"""This function takes a lists as an input and returns true if the given list is valid. 
-	The list will be a single block , single row or single column only. 
-	A valid list is defined as a list in which all non empty elements doesn't have a repeating element.
-	"""
-	# your code goes here
 	x=0
 	for i in lst:
 		if lst.count(i)>1 and i!=0:
@@ -136,9 +112,6 @@ def valid_list(lst: List[int])-> bool:
 		return True
 
 def valid_sudoku(sudoku:List[List[int]])-> bool:
-	"""This function returns True if the whole Sudoku is valid.
-	"""
-	# your code goes here
 	for row in sudoku:
 		if valid_list(row)==False:
 			return False
@@ -151,10 +124,6 @@ def valid_sudoku(sudoku:List[List[int]])-> bool:
 	return True
 
 def get_candidates(sudoku:List[List[int]], pos:Tuple[int, int]) -> List[int]:
-	"""This function takes position as argument and returns a list of all the possible values that 
-	can be assigned at that position so that the sudoku remains valid at that instant.
-	"""
-	# your code goes here
 	l=[]
 	A=get_block(sudoku,get_block_num(sudoku,pos))
 	B=get_row(sudoku,pos[0])
@@ -165,33 +134,14 @@ def get_candidates(sudoku:List[List[int]], pos:Tuple[int, int]) -> List[int]:
 	return l
 
 def make_move(sudoku:List[List[int]], pos:Tuple[int, int], num:int) -> List[List[int]]:
-	"""This function fill `num` at position `pos` in the sudoku and then returns
-	the modified sudoku.
-	"""
-	# your code goes here
 	sudoku[pos[0]-1][pos[1]-1]=num
 	return sudoku
 
 def undo_move(sudoku:List[List[int]], pos:Tuple[int, int]):
-	"""This function fills `0` at position `pos` in the sudoku and then returns
-	the modified sudoku. In other words, it undoes any move that you 
-	did on position `pos` in the sudoku.
-	"""
-	# your code goes here
 	sudoku[pos[0]-1][pos[1]-1]=0
 	return sudoku
 
 def sudoku_solver(sudoku: List[List[int]]) -> Tuple[bool, List[List[int]]]:
-	""" This is the main Sudoku solver. This function solves the given incomplete Sudoku and returns 
-	true as well as the solved sudoku if the Sudoku can be solved i.e. after filling all the empty positions the Sudoku remains valid.
-	It return them in a tuple i.e. `(True, solved_sudoku)`.
-
-	However, if the sudoku cannot be solved, it returns False and the same sudoku that given to solve i.e. `(False, original_sudoku)`
-	"""
-	# your code goes here
-
-	# to complete this function, you may define any number of helper functions.
-	# However, we would be only calling this function to check correctness.
 	og = sudoku.copy()
 	if valid_sudoku(sudoku)==False:
 		return (False,og)
@@ -221,12 +171,6 @@ def sudoku_solver(sudoku: List[List[int]]) -> Tuple[bool, List[List[int]]]:
 				undo_move(sudoku,pos)
 				return (False,sudoku)
 
-# PLEASE NOTE:
-# We would be importing your functions and checking the return values in the autograder.
-# However, note that you must not print anything in the functions that you define above before you 
-# submit your code since it may result in undefined behaviour of the autograder.
-# Following is the driver code
-# you can edit the following code to check your performance.
 if __name__ == "__main__":
 
 	# Input the sudoku from stdin
